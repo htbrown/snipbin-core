2f63deaedd52b61d10436e9563d3af5bfec94685
Hayden Brown <nedyah.brown@gmail.com>
2022-03-16 13:52:54 +0000

    to be tested: clean up snip endpoints

--- src/routes/v1.ts
+++ src/routes/v1.ts
@@ -1,9 +1,10 @@
 import { ApiResponse } from '../modules/types';
+import { logger, databaseManager } from "../index";
+import DatabaseManager from '../modules/databaseManager';
+import Utilities from '../modules/utilities';
 
 import { Router } from 'express';
 export let router = Router();
-
-import { databaseManager, logger } from '../index';
 
 router.get('/', (req, res) => {
     let response: ApiResponse = {
@@ -22,35 +23,23 @@ router.route('/snip')
     try {
         if (!req.query.id) {
             result = { status: 200, data: await databaseManager.models.snip.find().exec() };
-        } else if (databaseManager.checkId(req.query.id.toString())) {
+        } else if (DatabaseManager.checkId(req.query.id.toString())) {
             result = { status: 200, data: await databaseManager.models.snip.find({ _id: req.query.id }).exec() };
         } else {
-            result = { status: 400, error: true, data: 'Invalid snip ID' };
-            res.status(400);
+            Utilities.sendError(400, new Error('Invalid snip ID'), res);
+            return;
         }
     } catch (err) {
-        result = { status: 500, error: true, data: err.message };
-        res.status(500);
-        logger.log('express', 'ERROR: Something went wrong while returning a result.', err);
+        Utilities.sendError(500, err, res);
+        return;
     }
 
     res.send(result);
 })
 .post((req, res) => {
     let result: ApiResponse;
-    if (!req.query.name) {
-        result = { status: 400, error: true, data: 'No name provided' };
-        res.status(400);
-        res.send(result);
-        return;
-    }
-
-    if (req.query.user && !databaseManager.checkId(req.query.user.toString())) {
-        result = { status: 400, error: true, data: 'Invalid user ID' };
-        res.status(400);
-        res.send(result);
-        return;
-    }
+    if (!req.query.name) return Utilities.sendError(400, new Error('No name provided'), res);
+    if (req.query.user && !DatabaseManager.checkId(req.query.user.toString())) return Utilities.sendError(400, new Error('Invalid user ID'), res);
 
     let NewSnip = new databaseManager.models.snip({
         name: req.query.name,
@@ -62,22 +51,9 @@ router.route('/snip')
     });
 
     NewSnip.save((err: Error, snip: any) => {
-        if (err) {
-            result = { status: 500, error: true, data: err.message };
-            res.status(500);
-        }
+        if (err) return Utilities.sendError(500, err, res);
 
         result = { status: 200, data: snip, message: 'Created new snip' }
         res.send(result);
     });
-});
-
-// Tag Routes (GET, POST)
-
-router.route('/tag')
-.get((req, res) => {
-    res.send('to be implemented');
-})
-.post((req, res) => {
-    res.send('to be implemented');
 });
